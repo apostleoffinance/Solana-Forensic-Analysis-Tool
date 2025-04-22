55e2ba38b8b42ea5038c781d351a059b2b010916
0xJayOnchain <JayOnchain@proton.me>
2025-04-22 09:52:08 -0600

    Updating for gh-pages

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export', // Enables static export
+    basePath: isProd ? '/Solana-Forensic-Analysis-Tool' : '', // Replace <repo-name> with your GitHub repository name
+    assetPrefix: isProd ? '/Solana-Forensic-Analysis-Tool' : '', // Ensures assets are loaded correctly
+    images: {
+      unoptimized: true, // Disables Next.js image optimization (not supported in static export)
+    },
+    trailingSlash: true, // Adds trailing slashes to URLs (helps with GitHub Pages routing)
+  };
 
 export default nextConfig;
