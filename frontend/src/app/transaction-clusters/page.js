ba4cc83fe753c4b48cdf8f11f5c2a847bf9da178
0xJayOnchain <JayOnchain@proton.me>
2025-04-28 09:21:42 -0600

    Finsihign Transaction Clusters

--- frontend/src/app/transaction-clusters/page.js
+++ frontend/src/app/transaction-clusters/page.js
@@ -1,13 +1,37 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import TransactionClusters from '@/components/TransactionClusters'
-import { getMockTransactionClusters } from '@/api/transactionClusters'
+import { useData } from '@/app/DataContext'
 
 export default function TransactionClusterPage() {
-    const mockTransactionClustersData = getMockTransactionClusters()
+    const { data, error } = useData();
+      const [tx_graph, setTxGraph] = useState(null);
+      const [wallet_analysis, setWalletAnalysis] = useState(null);
+      const [loading, setLoading] = useState(true);
+    
+      useEffect(() => {
+        if (data) {
+          setTxGraph(data.tx_graph);
+          setWalletAnalysis(data.wallet_analysis);
+          setLoading(false);
+        }
+      }
+      , [data]);
+    
+      if (loading) {
+        return <div>Loading...</div>;
+      }
+      if (error) {
+        return <div>Error: {error}</div>;
+      }
+      if (!tx_graph || !wallet_analysis) {
+        return <div>No data available</div>;
+      }
+
   return (
     <div className="widget transaction-clusters">
       <h2>Transaction Clusters</h2>
-      <TransactionClusters clusters={mockTransactionClustersData} />
+      <TransactionClusters tx_graph={tx_graph} wallet_analysis={wallet_analysis} />
     </div>
   )
 }
